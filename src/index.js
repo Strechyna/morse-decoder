df1251032b65ca68ac9350d56c75004fc0b091b3
Strechyna <brouillasse@gmail.com>
2021-08-16 17:51:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,44 +1,66 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
+const BINARY_MORSE_TABLE = {
+    '10': '.',
+    '11': '-'
+};
+
+const SPACE = '**********';
+
 function decode(expr) {
-    // write your solution here
+    const binaryCharRegExp = new RegExp(`.{10}`, 'g');
+    const binaryChars = expr.match(binaryCharRegExp);
+    return binaryChars.map(getChar).join('');
+}
+
+function getChar(binaryChar) {
+    if (binaryChar === SPACE) return ' ';
+
+    return MORSE_TABLE[getMorceKey(binaryChar.replace(/^0+/, ''))];
+}
+
+function getMorceKey(binaryChar) {
+    const binaryMorseSymbolRegExp = new RegExp(`.{2}`, 'g');
+    const binaryMorseSymbols = binaryChar.match(binaryMorseSymbolRegExp);
+
+    return binaryMorseSymbols.map(val => BINARY_MORSE_TABLE[val]).join('');
 }
 
 module.exports = {
